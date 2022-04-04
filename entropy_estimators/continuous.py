40f8fc11141bb21293d1c6637806af26890f371c
daesungc <daesungc@gmail.com>
2022-04-04 14:43:27 +0200

    min_dist to 'auto' = smallest distance > 0.0

--- entropy_estimators/continuous.py
+++ entropy_estimators/continuous.py
@@ -160,65 +160,63 @@ def get_pmi_mvn(x, y, z):
 
 
 @convert_vectors_to_2d_arrays_if_any
-def get_h(x, k=1, norm='max', min_dist=0., workers=1):
-    """
-    Estimates the entropy H of a random variable x (in nats) based on
-    the kth-nearest neighbour distances between point samples.
-
-    @reference:
-    Kozachenko, L., & Leonenko, N. (1987). Sample estimate of the entropy of a random vector. Problemy Peredachi Informatsii, 23(2), 9–16.
-
-    Arguments:
-    ----------
-    x: (n, d) ndarray
-        n samples from a d-dimensional multivariate distribution
-
-    k: int (default 1)
-        kth nearest neighbour to use in density estimate;
-        imposes smoothness on the underlying probability distribution
-
-    norm: 'euclidean' or 'max'
-        p-norm used when computing k-nearest neighbour distances
-
-    min_dist: float (default 0.)
-        minimum distance between data points;
-        smaller distances will be capped using this value
-
+def get_h(x, k=1, norm='max', min_dist='auto', workers=1):
+	"""
+	Estimates the entropy H of a random variable x (in nats) based on
+	the kth-nearest neighbour distances between point samples.
+	@reference:
+	Kozachenko, L., & Leonenko, N. (1987). Sample estimate of the entropy of a random vector. Problemy Peredachi Informatsii, 23(2), 9–16.
+	Arguments:
+	----------
+	x: (n, d) ndarray
+		n samples from a d-dimensional multivariate distribution
+	k: int (default 1)
+		kth nearest neighbour to use in density estimate;
+		imposes smoothness on the underlying probability distribution
+	norm: 'euclidean' or 'max'
+		p-norm used when computing k-nearest neighbour distances
+	min_dist: float (default 'auto')
+		minimum distance between data points;
+		smaller distances will be capped using this value;
+		'auto' sets the min_dist as the smallest distance > 0
 	workers: int (default 1)
 		number of workers to use for parallel processing in query;
 		-1 uses all CPU threads
+	Returns:
+	--------
+	h: float
+		entropy H(X)
+	"""
 
-    Returns:
-    --------
-    h: float
-        entropy H(X)
-    """
+	n, d = x.shape
 
-    n, d = x.shape
+	if norm == 'max': # max norm:
+		p = np.inf
+		log_c_d = 0 # volume of the d-dimensional unit ball
+	elif norm == 'euclidean': # euclidean norm
+		p = 2
+		log_c_d = (d/2.) * log(np.pi) - log(gamma(d/2. +1))
+	else:
+		raise NotImplementedError("Variable 'norm' either 'max' or 'euclidean'")
 
-    if norm == 'max': # max norm:
-        p = np.inf
-        log_c_d = 0 # volume of the d-dimensional unit ball
-    elif norm == 'euclidean': # euclidean norm
-        p = 2
-        log_c_d = (d/2.) * log(np.pi) -log(gamma(d/2. +1))
-    else:
-        raise NotImplementedError("Variable 'norm' either 'max' or 'euclidean'")
+	kdtree = KDTree(x)
 
-    kdtree = KDTree(x)
+	# query all points -- k+1 as query point also in initial set
+	# distances, _ = kdtree.query(x, k + 1, eps=0, p=norm)
+	distances, _ = kdtree.query(x, k + 1, eps=0, p=p, workers=workers)
+	distances = distances[:, -1]
 
-    # query all points -- k+1 as query point also in initial set
-    # distances, _ = kdtree.query(x, k + 1, eps=0, p=norm)
-    distances, _ = kdtree.query(x, k + 1, eps=0, p=p, workers=workers)
-    distances = distances[:, -1]
+	# calculate min_dist as the smallest distances greater than 0.0
+	if min_dist == 'auto':
+		min_dist = np.unique(distances)[1]
 
-    # enforce non-zero distances
-    distances[distances < min_dist] = min_dist
+	# enforce non-zero distances
+	distances[distances < min_dist] = min_dist
 
-    sum_log_dist = np.sum(log(2*distances)) # where did the 2 come from? radius -> diameter
-    h = -digamma(k) + digamma(n) + log_c_d + (d / float(n)) * sum_log_dist
+	sum_log_dist = np.sum(log(2*distances)) # where did the 2 come from? radius -> diameter
+	h = -digamma(k) + digamma(n) + log_c_d + (d / float(n)) * sum_log_dist
 
-    return h
+	return h
 
 
 @convert_vectors_to_2d_arrays_if_any
